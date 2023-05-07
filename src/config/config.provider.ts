01a77d19c185a6a9a1a506f16a2809313e311167
Tomáš Hekrla (Heky) <tomas@heky.eu>
2023-05-07 11:42:19 +0200

    chore: replace eslint-plugin-node (#201)

--- src/config/config.provider.ts
+++ src/config/config.provider.ts
@@ -4,7 +4,7 @@ import { Config } from './types.js';
 export const configProvider: FactoryProvider<Config> = {
   provide: Config,
   useFactory: async () => {
-    // eslint-disable-next-line node/no-unsupported-features/es-syntax -- dynamic import is supported
+    // eslint-disable-next-line n/no-unsupported-features/es-syntax -- dynamic import is supported
     const module = await import('./index.js');
     return module.default;
   },
