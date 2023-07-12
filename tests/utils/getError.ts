865089b0132d22b1aeeb39d6194517d43790e938
Tomáš Hekrla (Heky) <tomas@heky.eu>
2023-07-12 23:19:56 +0200

    chore: fix eslint error

--- tests/utils/getError.ts
+++ tests/utils/getError.ts
@@ -7,7 +7,7 @@ class NoErrorThrownError extends Error {}
  * Returns the error thrown by the given function, or throws an error if no error was thrown.
  * https://github.com/jest-community/eslint-plugin-jest/blob/v26.8.7/docs/rules/no-conditional-expect.md#how-to-catch-a-thrown-error-for-testing-without-violating-this-rule
  */
-export const getError = async (runner: () => unknown | Promise<unknown>) => {
+export const getError = async (runner: () => unknown) => {
   try {
     await runner();
     throw new NoErrorThrownError();
