e388de3f183be58b044fa8caf6bef33e182175bf
Tomas Heky Hekrla <tomas@heky.eu>
2024-10-02 02:50:53 +0200

    Disable deploy (#432)
    * ci: remove deploy job
    
    * fix(config): loading
    
    * chore(gitignore): update config files

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,5 +2,9 @@ import { parseConfig } from './parse.js';
 
 export default async () => {
   const config = await import('config');
-  return await parseConfig(config);
+
+  return await parseConfig({
+    DEV: process.env['NODE_ENV'] !== 'production',
+    ...config.default,
+  });
 };
