14becc8b490d30ba159f85e17c8c3788b8663b01
Tomas Heky Hekrla <tomas@heky.eu>
2024-11-19 15:18:32 +0100

    feat: implement MxApiClient

--- dotnet/WATTrouter/MxApiClient.cs
+++ dotnet/WATTrouter/MxApiClient.cs
@@ -5,11 +5,29 @@ public class MxApiClient
 
   public async Task<AllTimeStats> GetAllTimeStatsAsync(Uri origin)
   {
-    throw new NotImplementedException();
+    using var httpClient = new HttpClient();
+    httpClient.BaseAddress = origin;
+
+    var response = await httpClient.GetAsync("stat_alltime.xml");
+
+    response.EnsureSuccessStatusCode();
+
+    var stream = await response.Content.ReadAsStreamAsync();
+
+    return AllTimeStats.Parse(stream);
   }
 
   public async Task<Measurement> GetMeasurementAsync(Uri origin)
   {
-    throw new NotImplementedException();
+    using var httpClient = new HttpClient();
+    httpClient.BaseAddress = origin;
+
+    var response = await httpClient.GetAsync("meas.xml");
+
+    response.EnsureSuccessStatusCode();
+
+    var stream = await response.Content.ReadAsStreamAsync();
+
+    return Measurement.Parse(stream);
   }
 }
