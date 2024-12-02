e54ae59d68fcd1cb63387539f6998f6c996b374c
Tomas Heky Hekrla <tomas@heky.eu>
2024-12-02 12:54:46 +0100

    fix: ObjectId

--- dotnet/App/ObjectId.cs
+++ dotnet/App/ObjectId.cs
@@ -1,8 +1,7 @@
 
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.CodeAnalysis;
 
 public class ObjectIdAttribute : RegularExpressionAttribute
 {
-  public ObjectIdAttribute() : base("[0-9a-z]{24}") {}
+  public ObjectIdAttribute() : base("[0-9a-f]{24}") {}
 }
