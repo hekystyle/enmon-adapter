3ffe2ce9ba0fadc254e75335bf36332f0f1ebcd0
Tomas Heky Hekrla <tomas@heky.eu>
2024-12-02 12:56:38 +0100

    fix: WATTrouter config

--- dotnet/App/WATTrouter/Config.cs
+++ dotnet/App/WATTrouter/Config.cs
@@ -1,12 +1,10 @@
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace WATTrouter;
 
 public record Config
 {
-  [DefaultValue("Mx")]
-  public string? Model { get; init; };
+  public required string Model { get; init; }
 
   [Url]
   public required Uri BaseURL { get; set; }
